f09fc43138fe569e33df0abc8a92b0ee871ff7a3
CharleszZ <sunchao.zb@ccbft.com>
2023-08-08 11:48:21 +0800

    first commit

---
+++ hjpj-backend/src/main/java/com/csun/hjpjbackend/HjpjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.csun.hjpjbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjpjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjpjBackendApplication.class, args);
+    }
+
+}
---
+++ hjpj-backend/src/test/java/com/csun/hjpjbackend/HjpjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csun.hjpjbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjpjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
